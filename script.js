bd956df1c6867310641e05440399ec507e6c88fb
jol-tm <joaolucastmagalhaes@gmail.com>
2024-11-26 16:47:05 -0300

    Adicionado minha chave da API e ajustes de responsividade

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", getData);
 
 async function getData() {
-    fetch("https://api.nasa.gov/planetary/apod?api_key=DEM_KEY&count=1")
+    fetch("https://api.nasa.gov/planetary/apod?api_key=9bdO3AvcQIGxbVDASdDndAZQZRUEYUgjh0PgSfuh&count=1")
     .then(Response => Response.json())
     .then(displayData)
     .catch(error);
@@ -14,15 +14,16 @@ function displayData(data) {
     const pic = document.querySelector("#pic");
 
     title.innerHTML = data[0].title;
-    pic.setAttribute("src", data[0].url);
+    pic.setAttribute("src", data[0].hdurl);
     
     pic.onload = () => {
         expl.innerHTML = data[0].explanation;
+        expl.style.display = "block";
         wrapper.classList.remove("glow");
     }
 }
 
 function error() {
     const wrapper = document.querySelector("#wrapper");
-    wrapper.innerHTML = "Error loading :c<br>Try refreshing the page,<br>but the API may be unavailable.";
+    wrapper.innerHTML = "Error loading :c<br>Try refreshing the page.";
 }
