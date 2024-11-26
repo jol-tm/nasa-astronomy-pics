cc9ddd45eafe19aad29c3d8c091dff600e390407
jol-tm <joaolucastmagalhaes@gmail.com>
2024-11-26 12:32:38 -0300

    Placeholder de carregamento, mensagem de erro, redução do tamanho do video de fundo

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-// document.addEventListener("DOMContentLoaded", getData);
+document.addEventListener("DOMContentLoaded", getData);
 
 async function getData() {
-    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1")
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEM_KEY&count=1")
     .then(Response => Response.json())
-    .then(displayData);
+    .then(displayData)
+    .catch(error);
 }
 
 function displayData(data) {
+    const wrapper = document.querySelector("#wrapper");
     const title = document.querySelector("#title");
     const expl = document.querySelector("#expl");
     const pic = document.querySelector("#pic");
 
     title.innerHTML = data[0].title;
-    expl.innerHTML = data[0].explanation;
-    pic.setAttribute("src", data[0].hdurl);
+    pic.setAttribute("src", data[0].url);
+    
+    pic.onload = () => {
+        expl.innerHTML = data[0].explanation;
+        wrapper.classList.remove("glow");
+    }
+}
+
+function error() {
+    const wrapper = document.querySelector("#wrapper");
+    wrapper.innerHTML = "Error loading :c<br>Try refreshing the page,<br>but the API may be unavailable.";
 }
