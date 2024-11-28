519cad6884a2ec22ad325e92b49bdd8ad4004aa8
jol-tm <joaolucastmagalhaes@gmail.com>
2024-11-28 13:03:04 -0300

    Consertado animação e tranbordamentos em textos muito grandes, porém ainda é necessário testes

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ function displayData(data) {
     const title = document.querySelector("#title");
     const expl = document.querySelector("#expl");
     const pic = document.querySelector("#pic");
-
+    
     title.innerHTML = data[0].title;
     expl.innerHTML = data[0].explanation;
     pic.setAttribute("src", data[0].hdurl);
+    
     
     pic.onload = () => {
         pic.classList.add("grow");
         expl.style.display = "block";
         wrapper.classList.remove("glow");
+
+        console.log(wrapper.scrollHeight  + ', '+ document.body.scrollHeight);
+
+        if (overflow(wrapper) <= 3) {
+            alert('O elemento está transbordando!');
+            wrapper.style.scale = 0.95;
+        } else if (overflow(wrapper) < 0) {
+            alert('O elemento está transbordando muito!');
+            wrapper.style.scale = 0.8;
+        }
     }
     
     pic.onerror = () => {
@@ -30,6 +41,10 @@ function displayData(data) {
         wrapper.innerHTML += "Error loading the image, sorry :c";
         wrapper.classList.remove("glow");
     }
+}
+
+function overflow(el) {
+    return (document.body.scrollHeight - el.scrollHeight);
 }
 
 function error() {
