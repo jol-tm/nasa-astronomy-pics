5d28813ea23cb425691129252215f52c7b80ea53
jol-tm <joaolucastmagalhaes@gmail.com>
2024-11-28 21:43:26 -0300

    Adicionado botão para reload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", getData);
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#changeImg").addEventListener("click", () => {
+        location.reload();
+    });
+});
 
 async function getData() {
     fetch("https://api.nasa.gov/planetary/apod?api_key=9bdO3AvcQIGxbVDASdDndAZQZRUEYUgjh0PgSfuh&count=1")
@@ -12,33 +17,33 @@ function displayData(data) {
     const title = document.querySelector("#title");
     const expl = document.querySelector("#expl");
     const pic = document.querySelector("#pic");
-    
+    const showExpl = () => {
+        expl.style.display = "block";
+        expl.classList.add("fade");
+        wrapper.classList.remove("glow");
+        expl.innerHTML = data[0].explanation; // O typeriter por algum motivo não funciona no onerror :c, ele recebe tudo certinho, até escreve no console mas não insere na tela, inserir assim de uma vez funciona
+        // typewrite(data[0].explanation, expl);   
+    }
+
     title.classList.add("fade");
     title.innerHTML = data[0].title;
     pic.setAttribute("src", data[0].hdurl, expl);
     
     pic.onload = () => {
+        showExpl();
         pic.classList.add("grow");
-        expl.style.display = "block";
-        expl.classList.add("fade");
-        typewrite(data[0].explanation, expl);
-        wrapper.classList.remove("glow");
-
         if (overflow(wrapper) <= 3) {
-            alert('O elemento está transbordando!');
             wrapper.style.scale = 0.95;
         } else if (overflow(wrapper) < 0) {
-            alert('O elemento está transbordando muito!');
             wrapper.style.scale = 0.8;
         }
     }
     
     pic.onerror = () => {
+        showExpl();
         pic.style.display = "none";
-        expl.style.display = "block";
         expl.style.maxWidth = "100%";
         wrapper.innerHTML += "Error loading the image, sorry :c";
-        wrapper.classList.remove("glow");
     }
 }
 
@@ -48,7 +53,6 @@ function overflow(el) {
 
 function typewrite(txt, dest) {
     let i = 0;
-
     const interval = setInterval(() => {
         if (i < txt.length) {
             dest.innerHTML += txt[i];
